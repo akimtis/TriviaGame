c6b355fdd963f7f25ea5c1c9870f32ada1b0fa00
Anna Kimtis <Anna@Annas-MacBook-Pro.local>
2016-12-27 12:49:09 -0600

    improved js document in an attempt at the simpler version of the trivia game

--- javascript/app.js
+++ javascript/app.js
@@ -12,152 +12,124 @@
 //12. repeat steps 7-11 until the arrays have been run through
 //13. display final screen with correct guesses, incorrect guesses, number of unanswered guesses, and start over button.
 //14. page does not reload on clicking start over, but resets the game instead.
+$('#start').on("click", function(){
+  trivia.start();
+})
 
-
-var correct = 0;
-var incorrect = 0;
-var unanswered = 0;
-var timerNumber = 30;
-var counter;
-var questionNumber = 0;
-var userClick = $(".userChoice").on("click", this)
+$(document).on('click', '#finished', function(){
+  trivia.done();
+})
 
 var questions = [{
     question: "1) The Chihuahuan Desert has...",
-    choices: ["the greatest number of aquatic species of any desert",
+    answers: ["the greatest number of aquatic species of any desert",
       "the greatest number of mammal species of any desert",
       "the greatest number of reptile species of any desert",
       "the greatest number of plant speices of any desert"],
-    answer: 1
+    correctanswer: "the greatest number of aquatic species of any desert"
 }, {
     question: "2) The Chihuahuan Desert is located ...",
-    choices: ["beside a small annoying dog",
+    answers: ["beside a small annoying dog",
       "in the state of Chihuahua, Mexico, and parts of the southern New Mexico",
       "in Canada",
       "mostly in California and Arizona"],
-    answer: 1
+    correctanswer: "in the state of Chihuahua, Mexico, and parts of the southern New Mexico"
 }, {
     question: "3) The defense mechanisms of the Texas Horned Lizard does NOT include ...",
-    choices: ["spines on its body",
+    answers: ["spines on its body",
       "the ability to camoflage with desert sand",
       "the ability to shoot blood from its eyes",
       "a bacteria-laden bite"],
-    correctAnswer: 3
+    correctanswer: "the ability to shoot blood from its eyes"
 }, {
     question: "4) Cactus have the following adaptations for saving water EXCEPT...",
-    choices: ["Preforming photosynthesis at night",
+    answers: ["Preforming photosynthesis at night",
       "Attracting animals to live in thier structure",
       "Expanding their sturcture to store water"],
-    answer: 1
+    correctanswer: "Attracting animals to live in thier structure"
 }, {
     question: "5) Many of the animals in the desert are active at twilight. This means they are...",
-    choices: ["nocturnal", "diurnal", "crepuscular", "hungry"],
-    answer: 2
+    answers: ["nocturnal", "diurnal", "crepuscular", "sleepy"],
+    correctanswer: "crepuscular"
 }];
 
-var currentQuestion = function() {
-   runTimer()
-    $("#questions").append(questions[questionNumber].question);
-    $("#choices1").append(questions[questionNumber].choices[0]);
-    $("#choices2").append(questions[questionNumber].choices[1]);
-    $("#choices3").append(questions[questionNumber].choices[2]);
-    $("#choices4").append(questions[questionNumber].choices[3]);
-  //var timeout = setTimeout(function(),30000);
-} 
-
-var currentAnswer = function() {
-
-  $("#questions").append(questions[num].answer);
-  // $(".userChoice").empty();
-  $("#answers").append("Next Question");
 
 
-//  if (userClick == questions[num].answer) {
-//   wins++;
-//   console.log("Correct!");
- 
-//   }
-  
-//   if (userClick != questions[num].answer) {
-//   losses++;
-//   console.log("Incorrect!");
-//   }
-  
-//   if (timeout) {
-//     unanswered ++;
-//   } 
+// var trivia = {
+//   counter: 30,
+//   correct: 0,
+//   incorrect: 0,
+//   countdown: function(){
+//     trivia.counter--;
+//     $('#counter').html(trivia.counter);
+//     if(trivia.counter<=0){
+//       console.log("time is up!");
+//       trivia.done();
+//     }
+//   },
+  start: function(){
+    timer = setInterval(trivia.countdown, 1000);
+    $('subwrapper').append('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>');
+    $('#start').on("click", function(){
+      console.log("clicked")
+      $('#start').remove();
+        for (var i=0; i<questions.length; i++){
+        $('subwrapper').append('<h2>'+questions[i].question+'</h2>');
+          for(var j=0; j<questions[i].answers.length; j++){
+          $('subwrapper').append("<input type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>"+questions[i].answers[j])
+          }
+        };
+    console.log(questions[i]);
+    })
+//     $('#subwrapper').append('<br><button id="finished">Finished</button>');
+//   },
+//   done: function(){
+//     $.each($('input[name="question-0"]:checked'), function(){
+//       if($(this).val()==questions[0].correctanswer){
+//         game.correct++;
+//       } else {
+//         game.incorrect++;
+//       }
+//     });
+//     $.each($('input[name="question-1"]:checked'), function(){
+//       if($(this).val()==questions[1].correctanswer){
+//         game.correct++;
+//       } else {
+//         game.incorrect++;
+//       }
+//     });
+//     $.each($('input[name="question-2"]:checked'), function(){
+//       if($(this).val()==questions[2].correctanswer){
+//         game.correct++;
+//       } else {
+//         game.incorrect++;
+//       }
+//     });
+//     $.each($('input[name="question-3"]:checked'), function(){
+//       if($(this).val()==questions[3].correctanswer){
+//         game.correct++;
+//       } else {
+//         game.incorrect++;
+//       }
+//     });
+//     $.each($('input[name="question-4"]:checked'), function(){
+//       if($(this).val()==questions[4].correctanswer){
+//         game.correct++;
+//       } else {
+//         game.incorrect++;
+//       }
+//     });
+//     this.result();
+//   },
+// result: function(){
+//   clearInterval(timer);
+//   $('#subwrapper h2').remove();
+//   $('#subwrapper').html("<h2> You Are Finished </h2>");
+//   $('#subwrapper').append("<h3>Correct Answers: "+this.correct+"</h3>");
+//   $('#subwrapper').append("<h3>Incorrect Answers: "+this.correct+"</h3>");
+//   $('#subwrapper').append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"</h3>");
 // }
-}
+// }
 
-
-var runTimer = function() {
-      counter = setInterval(decrement, 1000);
-    }
-    //  The decrement function.
-    function decrement() {
-      //  Decrease number by one.
-      timerNumber--;
-      //  Show the number in the #timer tag.
-      $("#timer").html("<h2>" + "Time Left: " + timerNumber + " seconds" + "</h2>");
-      //  Once number hits zero...
-      if (timerNumber === 0) {
-        //  ...run the stop function.
-        stop();
-        
-      }
-    }
-
-function stop() {
-      //  Clears our "counter" interval.
-      //  We just pass the name of the interval
-      //  to the clearInterval function.
-      clearInterval(counter);
-    }
-//on click listen and run funciton runQuiz, which creates divs for each question
-//and buttons for each choice 
-
-$(document).ready(function(){
-
-$("#start").on("click", currentQuestion);
-
-$(".userChoice").on("click", currentAnswer);
-
-})
-
-
-
-// function runQuiz(){
-          
-
-  //$(#questions).empty();
-
-  // for (i=0; i<questions.length; i++) {
-  //   var a = $("<h1>");
-  //     // Adding a class of movie to our button
-  //         a.addClass("question");
-  //         // Adding a data-attribute
-  //         a.attr("data-name", questions[i].question);
-  //         // Providing the initial button text
-  //         a.text(questions[i]);
-  //         // Adding the button to the HTML
-  //         $("#questions").append(a);
-  //         console.log(questions[i].question)
-  //   }
-  
-  // for(j=0; j<questions.choices.length; j++) {
-  //   var b = $("<button>");
-  //     // Adding a class of movie to our button
-  //         b.addClass("choice");
-  //         // Adding a data-attribute
-  //         b.attr("data-name", choices[j]);
-  //         // Providing the initial button text
-  //         b.text(choices[j]);
-  //         // Adding the button to the HTML
-  //         $("#questions").appendChild(b);
-  //     }
-
-
-
- 
 
 
